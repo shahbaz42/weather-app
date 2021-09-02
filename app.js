54b5ce8d7afa7c0f2c8f812373b0ca41803c65df
Shahbaz <84879814+shahbaz42@users.noreply.github.com>
2021-09-02 21:41:44 +0000

    Removed unwanted code from app.get /display

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+const mySecret = process.env['APP_ID']
+
 const express = require("express");  //express
 const https = require("https");      //https for making requets to api
 const ejs = require("ejs");          // for ejs templating
@@ -11,27 +14,12 @@ app.use(bodyParser.urlencoded({extended: true}));  //linking bodyparser with exp
 app.set("view engine", "ejs");
 
 app.get("/display", function(req, res){
-  const cityName = "Delhi";     
-  const appId = process.env.APP_ID;
-  const units = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+appId+"&units="+units;  //this url will be used to call openweather api
-
-  https.get(url, function(response){
-    response.on("data", function(data){       //returns data in hexa-desimal format
-        const weatherData = JSON.parse(data);   // to parse data in text format  //JSON.stringify(weatherData);     //This does opposite of JSON.parse this removes spaces and stringify weatherData
-        weatherData['date']=dateProvider.getDate(weatherData);
-        if (weatherData.cod == 200){
-          res.render("home", weatherData);  
-        }else{
-          res.render("not-found");
-        }
-    });
-  });
-});
+  res.redirect("/");
+})
 
 app.post("/display", function(req, res){
   const cityName = req.body.cityName;         //recieving cityName from form
-  const appId = process.env.APP_ID;
+  const appId = mySecret;
   const units = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+appId+"&units="+units;  //this url will be used to call openweather api
 
